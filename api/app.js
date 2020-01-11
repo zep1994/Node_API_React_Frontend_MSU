42df3778da3493856656fc8fed434a1a179e2744
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2020-01-11 07:19:44 -0600

    Init api req

--- api/app.js
+++ api/app.js
@@ -1,16 +1,24 @@
 const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+//Define routes
 let indexPage = require('./routes/index')
+let msu_api = require('./routes/msu_api')
 
+//Temp for views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.use(express.json())
+// MiddleWare
+app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
-app.use('/', indexPage)
 
+// Routes
+//app.use('/', indexPage)
+
+//Port
 app.listen(3001)
 
